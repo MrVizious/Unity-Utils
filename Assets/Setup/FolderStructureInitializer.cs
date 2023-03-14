01e629c1f9c152ac3e14dfa83e3fe0176a05ddb4
mrvizious <mrvizious@gmail.com>
2023-03-14 16:57:11 +0100

    Folder Structure Initializer

---
+++ Assets/Setup/FolderStructureInitializer.cs
@@ -0,0 +1,33 @@
+#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public static class FolderStructureInitializer
+{
+    static FolderStructureInitializer()
+    {
+        if (!SessionState.GetBool("FirstInitDone", false))
+        {
+            // Startup code here...
+            Debug.Log("First Init.");
+            FolderCreation();
+
+            SessionState.SetBool("FirstInitDone", true);
+        }
+    }
+
+    private static void FolderCreation()
+    {
+        if (AssetDatabase.IsValidFolder("Assets/_Home_")) return;
+        AssetDatabase.CreateFolder("Assets", "_Home_");
+        AssetDatabase.CreateFolder("Assets/_Home_", "Art");
+        AssetDatabase.CreateFolder("Assets/_Home_", "Events");
+        AssetDatabase.CreateFolder("Assets/_Home_", "Scenes");
+        AssetDatabase.CreateFolder("Assets/_Home_", "Scriptable Object Instances");
+        AssetDatabase.CreateFolder("Assets/_Home_", "Scripts");
+    }
+}
+#endif
