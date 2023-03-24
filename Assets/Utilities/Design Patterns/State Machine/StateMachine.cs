66cb6c8962e032d6f4d57f35798a3140c2259108
mrvizious <mrvizious@gmail.com>
2023-03-24 16:05:01 +0100

    Improved state machine

--- Assets/Utilities/Design Patterns/State Machine/StateMachine.cs
+++ Assets/Utilities/Design Patterns/State Machine/StateMachine.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace DesignPatterns
 {
@@ -10,8 +11,11 @@ namespace DesignPatterns
     /// <typeparam name="T">State type</typeparam>
     public abstract class StateMachine<T> : MonoBehaviour where T : State<T>
     {
-        public Stack<T> stateStack { get; protected set; }
+        [ShowInInspector]
+        public Stack<T> stateStack { get; set; }
+        [ShowInInspector]
         public T currentState { get; protected set; }
+        public T previousState { get { return stateStack.Peek(); } }
 
         protected virtual void Awake()
         {
@@ -19,7 +23,31 @@ namespace DesignPatterns
         }
         public virtual void ChangeToState(T newState)
         {
+            // null check
             if (newState == null) Debug.LogError("New State to change into is null!");
+
+            // no current state
+            if (currentState == null)
+            {
+                currentState = newState;
+                currentState.Enter(this);
+                return;
+            }
+
+            // trying to change into current state
+            if (currentState.Equals(newState)) return;
+
+            // previous state is same as new one
+            if (stateStack.Count > 0 && previousState.Equals(newState))
+            {
+                currentState?.Exit();
+                stateStack.Pop();
+                stateStack.Push(currentState);
+                currentState = newState;
+                currentState.Enter(this);
+                return;
+            }
+
             currentState?.Exit();
             stateStack.Push(currentState);
             currentState = newState;
