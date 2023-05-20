7280a1d37b1eaf7f938562932ce5b8bdaff1e648
mrvizious <mrvizious@gmail.com>
2023-05-20 21:31:17 +0200

    Modified Components folder created, Image with transparency added for raycasts

---
+++ Assets/Modified Components/NonRaycastableTransparencyImage.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ExtensionMethods;
+
+public class NonRaycastableTransparencyImage : Image
+{
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        Image img = GetComponent<Image>();
+        if (img == null) return;
+        img.alphaHitTestMinimumThreshold = 0.001f;
+    }
+    public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    {
+        bool result = base.IsRaycastLocationValid(screenPoint, eventCamera);
+        if (!result)
+        {
+            return false;
+        }
+        if (this.fillMethod == FillMethod.Radial360)
+        {
+            Vector2 localPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, screenPoint, eventCamera, out localPoint);
+            Vector2 radialFillOriginVector;
+            switch (this.fillOrigin)
+            {
+                // Bottom
+                default:
+                    radialFillOriginVector = Vector2.down;
+                    break;
+                // Right
+                case 1:
+                    radialFillOriginVector = Vector2.right;
+                    break;
+                // Top
+                case 2:
+                    radialFillOriginVector = Vector2.up;
+                    break;
+                // Left
+                case 3:
+                    radialFillOriginVector = Vector2.left;
+                    break;
+            }
+
+            float clickAngle = 0f;
+            if (fillClockwise)
+            {
+                clickAngle = radialFillOriginVector.Angle360To(localPoint);
+            }
+            else
+            {
+                clickAngle = localPoint.Angle360To(radialFillOriginVector);
+            }
+            if (clickAngle <= fillAmount * 360f) return true;
+            return false;
+        }
+
+        // TODO: Add support for other fillable image types
+        return false;
+    }
+}
