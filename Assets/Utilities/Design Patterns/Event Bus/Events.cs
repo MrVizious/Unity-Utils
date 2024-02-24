c3db2197dc48c7c96105702236b715e4f4db2849
Cristian Ros <133830154+CristianRos@users.noreply.github.com>
2024-02-24 13:26:47 +0100

    Moved test event outside Events.cs

--- Assets/Utilities/Design Patterns/Event Bus/Events.cs
+++ Assets/Utilities/Design Patterns/Event Bus/Events.cs
@@ -3,14 +3,4 @@ using System;
 namespace DesignPatterns.EventBus
 {
     public interface IEvent { }
-
-    #region ShirtCreationMinigame
-
-    public struct ToolClickedEvent : IEvent
-    {
-        public ToolData toolData;
-        public Type toolType;
-    }
-
-    #endregion
 }
