64fc24413a27e29608ecee0c584c901336cbd208
Javier Riera Chirivella <mrvizious@gmail.com>
2024-04-11 16:37:11 +0200

    More UniTask methods

--- Assets/Utilities/Utility Methods/UniTaskMethods.cs
+++ Assets/Utilities/Utility Methods/UniTaskMethods.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 
 namespace UtilityMethods
@@ -33,6 +34,42 @@ namespace UtilityMethods
             }
             await UniTask.WhenAll(tempTaskList);
             tempTaskList.Clear();
+        }
+
+        public static async UniTask<T> ExecuteAndRaiseCancelIfValue<T>(UniTask<T> task, CancellationTokenSource cancellationToken, T valueToCancelOn)
+        {
+            var result = await task;
+            if (EqualityComparer<T>.Default.Equals(result, valueToCancelOn))
+            {
+                cancellationToken.Cancel();
+                throw new OperationCanceledException(cancellationToken.Token);
+            }
+            return result;
+        }
+
+        public static async UniTask WhenAllUnlessValue<T>(IEnumerable<UniTask<T>> tasks, T valueToCancelOn)
+        {
+            var cts = new CancellationTokenSource();
+
+            foreach (UniTask<T> task in tasks)
+            {
+                await ExecuteAndRaiseCancelIfValue<T>(task, cts, valueToCancelOn);
+            }
+
+            try
+            {
+                await UniTask.WhenAll(tasks);
+                // If we reach here, both tasks completed successfully without null results
+            }
+            catch (OperationCanceledException e)
+            {
+                throw e;
+            }
+        }
+
+        public static UniTask<TResult> WrapTaskWithCast<TOriginal, TResult>(UniTask<TOriginal> task) where TOriginal : TResult
+        {
+            return task.ContinueWith(t => (TResult)t);
         }
     }
 }
