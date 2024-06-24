a89a3f6cc201c517b8f940846cd3ff24e4e9fb33
Cristian Ros <133830154+CristianRos@users.noreply.github.com>
2024-06-24 20:52:48 +0200

    Commented EventBus debug messages

--- Assets/Utilities/Design Patterns/Event Bus/EventBusUtil.cs
+++ Assets/Utilities/Design Patterns/Event Bus/EventBusUtil.cs
@@ -66,7 +66,7 @@ namespace DesignPatterns.EventBus
             {
                 var busType = typedef.MakeGenericType(eventType);
                 eventBusTypes.Add(busType);
-                Debug.Log($"Initialized EventBus<{eventType.Name}>");
+                // Debug.Log($"Initialized EventBus<{eventType.Name}>");
             }
 
             return eventBusTypes;
@@ -77,7 +77,7 @@ namespace DesignPatterns.EventBus
         /// </summary>
         public static void ClearAllBuses()
         {
-            Debug.Log("Clearing all buses...");
+            // Debug.Log("Clearing all buses...");
             for (int i = 0; i < EventBusTypes.Count; i++)
             {
                 var busType = EventBusTypes[i];
