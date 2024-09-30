0ef90a8f1ca489cefa4b58523f2f5f9e43e92554
MrVizious <mrvizious@gmail.com>
2024-09-30 19:00:14 +0200

    Better WrapperSO

--- Assets/Custom Classes/WrapperSO.cs
+++ Assets/Custom Classes/WrapperSO.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 
 
 
-//[CreateAssetMenu(fileName = "WrapperSO", menuName = "Unity-Utils/WrapperSO", order = 0)]
-public class WrapperSO<T> : SerializedScriptableObject where T : new()
+//[CreateAssetMenu(fileName = "WrapperSO", menuName = "Unity-Utils/WrapperClassSO", order = 0)]
+public class WrapperClassSO<T> : SerializedScriptableObject where T : new()
 {
     public T content = new T();
-    public static implicit operator T(WrapperSO<T> wrapperSO) => wrapperSO.content;
+    public static implicit operator T(WrapperClassSO<T> wrapperSO) => wrapperSO.content;
+}
+/*
+Example usage:
+
+[CreateAssetMenu(fileName = "MyClassWrapperSO", menuName = "Unity-Utils/Wrappers/WrapperClassSO", order = 0)]
+public class MyClassWrapperSO : WrapperClassSO<int> {}
+*/
+
+
+
+//[CreateAssetMenu(fileName = "WrapperSO", menuName = "Unity-Utils/WrapperStructSO", order = 0)]
+public class WrapperStructSO<T> : SerializedScriptableObject where T : struct
+{
+    public T content;
+    public static implicit operator T(WrapperStructSO<T> wrapperSO) => wrapperSO.content;
 }
 
 /*
 Example usage:
 
-[CreateAssetMenu(fileName = "IntWrapperSO", menuName = "Unity-Utils/Wrappers/WrapperSO", order = 0)]
-public class IntWrapperSO : WrapperSO<int> {}
+[CreateAssetMenu(fileName = "MyStructWrapperSO", menuName = "Unity-Utils/Wrappers/WrapperStructSO", order = 0)]
+public class MyStructWrapperSO : WrapperStructSO<int> {}
 */
