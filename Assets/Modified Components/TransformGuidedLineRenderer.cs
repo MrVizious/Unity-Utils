c79b440097fc474c84a6ef4c5053e2cf9783f8f7
Javier Riera Chirivella <mrvizious@gmail.com>
2024-10-10 12:37:33 +0200

    Transform guided linerenderer

---
+++ Assets/Modified Components/TransformGuidedLineRenderer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+
+[RequireComponent(typeof(LineRenderer))]
+public class TransformGuidedLineRenderer : SerializedMonoBehaviour
+{
+    private LineRenderer _lineRenderer;
+    private LineRenderer lineRenderer
+    {
+        get
+        {
+            if (_lineRenderer == null) TryGetComponent<LineRenderer>(out _lineRenderer);
+            return _lineRenderer;
+        }
+    }
+
+    public List<Transform> transforms = new List<Transform>();
+
+    private void Awake()
+    {
+        lineRenderer.positionCount = 0;
+    }
+    private void Update()
+    {
+        Vector3[] positions = new Vector3[transforms.Count];
+        for (int i = 0; i < transforms.Count; i++)
+        {
+            positions[i] = transforms[i].position;
+        }
+        lineRenderer.positionCount = transforms.Count;
+        lineRenderer.SetPositions(positions);
+    }
+
+}
