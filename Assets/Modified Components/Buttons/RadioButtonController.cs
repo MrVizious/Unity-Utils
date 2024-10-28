e06453d2b0718ea3b12064aefb70b811e7a64392
Javier Riera Chirivella <mrvizious@gmail.com>
2024-10-28 15:49:06 +0100

    Serialized radio buttons

--- Assets/Modified Components/Buttons/RadioButtonController.cs
+++ Assets/Modified Components/Buttons/RadioButtonController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
 using UnityEngine;
 
-public class RadioButtonController : MonoBehaviour
+public class RadioButtonController : SerializedMonoBehaviour
 {
-    private List<OnOffExtendedButton> buttons = new List<OnOffExtendedButton>();
+    [OdinSerialize]
+    private HashSet<OnOffExtendedButton> buttons = new HashSet<OnOffExtendedButton>();
 
     private void OnClicked(OnOffExtendedButton button)
     {
@@ -17,12 +20,18 @@ public class RadioButtonController : MonoBehaviour
 
     public void Subscribe(OnOffExtendedButton button)
     {
-        buttons.Add(button);
-        button.onClick += () => OnClicked(button);
+        Debug.Log($"Subcribing button {button.gameObject}", this);
+        if (buttons.Add(button))
+            button.onClick += () => OnClicked(button);
     }
     public void Unsubscribe(OnOffExtendedButton button)
     {
-        buttons.Remove(button);
-        button.onClick -= () => OnClicked(button);
+        if (buttons.Remove(button))
+            button.onClick -= () => OnClicked(button);
+    }
+
+    private void OnDestroy()
+    {
+        buttons.Clear();
     }
 }
