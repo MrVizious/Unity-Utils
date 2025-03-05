85b59c37b8bc1dadaf7d4ff85760b19e54e89774
MrVizious <mrvizious@gmail.com>
2025-03-05 16:01:37 +0100

    Texture decompress

--- Assets/Utilities/Extension Methods/TextureExtensionMethods.cs
+++ Assets/Utilities/Extension Methods/TextureExtensionMethods.cs
@@ -77,5 +77,25 @@ namespace ExtensionMethods
             return source.Scale(newWidth, newHeight);
         }
 
+        public static Texture2D DeCompress(this Texture2D source)
+        {
+            RenderTexture renderTex = RenderTexture.GetTemporary(
+                        source.width,
+                        source.height,
+                        0,
+                        RenderTextureFormat.Default,
+                        RenderTextureReadWrite.Linear);
+
+            Graphics.Blit(source, renderTex);
+            RenderTexture previous = RenderTexture.active;
+            RenderTexture.active = renderTex;
+            Texture2D readableText = new Texture2D(source.width, source.height);
+            readableText.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+            readableText.Apply();
+            RenderTexture.active = previous;
+            RenderTexture.ReleaseTemporary(renderTex);
+            return readableText;
+        }
+
     }
 }
