34acf142573084110acc36dda8fb35262d3f3b08
MrVizious <mrvizious@gmail.com>
2025-03-28 14:06:34 +0100

    Directive added

--- Assets/Setup/OdinNetworkBehaviourEditor.cs
+++ Assets/Setup/OdinNetworkBehaviourEditor.cs
@@ -1,8 +1,10 @@
 #if ODIN_INSPECTOR
+#if UNITY_NETCODE || UNITY_NETCODE_GAMEOBJECTS
 using Sirenix.OdinInspector.Editor;
 using Unity.Netcode;
 using UnityEditor;
 
 [CustomEditor(typeof(NetworkBehaviour), true)]
 public class OdinNetworkBehaviourEditor : OdinEditor { }
+#endif
 #endif
