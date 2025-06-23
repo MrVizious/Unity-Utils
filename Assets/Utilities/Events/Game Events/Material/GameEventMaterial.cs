d419be5f0f6e8ff26314da7598a193e750ab9c00
MrVizious <mrvizious@gmail.com>
2025-06-23 23:50:28 +0100

    Game events added

---
+++ Assets/Utilities/Events/Game Events/Material/GameEventMaterial.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GameEvents
+{
+    [CreateAssetMenu(menuName = "GameEvents/Material")]
+    public class GameEventMaterial : GameEventTyped<Material>
+    {
+
+    }
+}
