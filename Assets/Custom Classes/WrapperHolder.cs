aa2308d3b7a1f27218b6415a4ec72b5c211d5d13
MrVizious <mrvizious@gmail.com>
2025-06-25 18:49:01 +0100

    WrapperHolder

---
+++ Assets/Custom Classes/WrapperHolder.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class WrapperHolder<T>
+{
+    [SerializeField]
+    public WrapperSO<T> dataWrapperSO;
+    private T _content = Activator.CreateInstance<T>();
+
+    [SerializeField]
+    public T content
+    {
+        get
+        {
+            if (dataWrapperSO != null) return dataWrapperSO.content;
+            return _content;
+        }
+        set
+        {
+            if (dataWrapperSO != null) dataWrapperSO.content = value;
+            _content = value;
+        }
+    }
+
+    public WrapperHolder() { }
+}
