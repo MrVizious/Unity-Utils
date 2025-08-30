88c9b53f08b97a51095c2778a3fa3207c0d2fbf5
MrVizious <mrvizious@gmail.com>
2025-08-30 12:22:22 +0100

    Local Reset

--- Assets/Utilities/Extension Methods/TransformExtensionMethods.cs
+++ Assets/Utilities/Extension Methods/TransformExtensionMethods.cs
@@ -20,6 +20,12 @@ namespace ExtensionMethods
             transform.localRotation = Quaternion.identity;
             transform.localScale = Vector3.one;
         }
+        public static void LocalReset(this Transform transform)
+        {
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+            transform.localScale = Vector3.one;
+        }
 
         public static void SetChildrenActive(this Transform transform, bool newValue)
         {
