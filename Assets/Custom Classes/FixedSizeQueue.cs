75be9bb751c15b6295afd8d6b2380b3c236354ee
MrVizious <mrvizious@gmail.com>
2025-08-30 12:20:28 +0100

    FixedSizeQueue

---
+++ Assets/Custom Classes/FixedSizeQueue.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+public class FixedSizedQueue<T> : Queue<T>
+{
+    public int Size { get; private set; }
+
+    public FixedSizedQueue(int size)
+    {
+        Size = size;
+    }
+
+    public new void Enqueue(T obj)
+    {
+        Enqueue(obj);
+        while (Count > Size)
+        {
+            T outObj;
+            TryDequeue(out outObj);
+        }
+    }
+}
